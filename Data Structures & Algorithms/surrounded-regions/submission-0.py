30cdda0d2a95e859b8749c13feee4f7999eb7d16
mvesely296 <151570610+mvesely296@users.noreply.github.com>
2026-07-09 18:14:50 +0200

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,41 @@
+class Solution:
+    def __init__(self):
+        self.rows = 0
+        self.cols = 0
+        self.dirs = (
+            (-1, 0),
+            (1, 0),
+            (0, -1),
+            (0, 1)
+        )
+
+    def _check_surrounded_from_point(self, i: int, j: int, seen: set[tuple[int, int]], board: list[list[str]]) -> bool:
+        if min(i, j) < 0 or i >= self.rows or j >= self.cols:
+            return False
+        if board[i][j] == "X" or (i, j) in seen:
+            return True
+
+        seen.add((i, j))
+
+        return all(self._check_surrounded_from_point(i+dx, j+dy, seen, board) for dx, dy in self.dirs)
+
+
+
+    def solve(self, board: list[list[str]]) -> None:
+        seen: set[tuple[int, int]] = set()
+
+        self.rows = len(board)
+        self.cols = len(board[0])
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if (i, j) in seen or board[i][j] == "X":
+                    continue
+
+                current_seen = set()
+
+                if self._check_surrounded_from_point(i, j, current_seen, board):
+                    for surrounded_x, surrounded_y in current_seen:
+                        board[surrounded_x][surrounded_y] = "X"
+                else:
+                    seen |= current_seen
